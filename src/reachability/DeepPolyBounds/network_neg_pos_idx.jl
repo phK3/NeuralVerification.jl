1024a02186c521d065325446e6122420db33304e
phK3 <ufedz@student.kit.edu>
2021-10-27 16:33:07 +0200

    Network that stores positive and negative weights as well as indices for layers

---
+++ src/reachability/DeepPolyBounds/network_neg_pos_idx.jl
@@ -0,0 +1,31 @@
+
+############# Network with [W]⁺, [W]⁻ and layers that store their index ########
+
+struct LayerNegPosIdx{F<:ActivationFunction, N<:Number}
+    weights::Matrix{N}
+    bias::Vector{N}
+    W_neg::Matrix{N}
+    W_pos::Matrix{N}
+    activation::F
+    index::Int64
+end
+
+LayerNegPosIdx(L::Layer, idx::Int64) = LayerNegPosIdx(L.weights, L.bias, min.(L.weights, 0), max.(L.weights, 0),
+                                                           L.activation, idx)
+n_nodes(L::LayerNegPosIdx) = length(L.bias)
+affine_map(L::LayerNegPosIdx, x)  = L.weights*x .+ L.bias
+
+
+struct NetworkNegPosIdx <: AbstractNetwork
+    layers::Vector{LayerNegPosIdx}
+end
+
+NetworkNegPosIdx(net::Network) = NetworkNegPosIdx([LayerNegPosIdx(L, i) for (i, L) in enumerate(net.layers)])
+
+function compute_output(nnet::NetworkNegPosIdx, input)
+    curr_value = input
+    for layer in nnet.layers
+        curr_value = layer.activation(affine_map(layer, curr_value))
+    end
+    return curr_value
+end

f4112f45ea03f8f93c7915bebf0a020de499abab
phK3 <ufedz@student.kit.edu>
2022-02-04 17:14:21 +0100

    Merge whole constraints matrix into last layer

--- src/reachability/DeepPolyBounds/utils.jl
+++ src/reachability/DeepPolyBounds/utils.jl
@@ -18,7 +18,7 @@ function merge_into_network(network::Network, coeffs::Vector{N} where N<:Number)
     l = network.layers[end]
     if typeof(l.activation) == Id
         Ŵ = coeffs' * l.weights
-        b̂ =coeffs' * l.bias
+        b̂ = coeffs' * l.bias
         push!(layers, Layer(Array(Ŵ), [b̂], Id()))
     elseif typeof(l.activation) == ReLU
         Ŵ = copy(l.weights)
@@ -26,6 +26,36 @@ function merge_into_network(network::Network, coeffs::Vector{N} where N<:Number)
         push!(layers, Layer(Ŵ, b̂, ReLU()))
         # just add an additional layer with the linear objective
         push!(layers, Layer(Array(coeffs'), zeros(1), Id()))
+    else
+        throw(DomainError(l.activation, ": the activation function is not supported!"))
+    end
+
+    return Network(layers)
+end
+
+
+# TODO: merge upper function for vectors coeffs into this function
+function NV.merge_into_network(network::Network, A::Matrix{N}, b::Vector{N}) where N<:Number
+    layers = []
+    for l in network.layers[1:end-1]
+        Ŵ = copy(l.weights)
+        b̂ = copy(l.bias)
+        σ = l.activation
+        push!(layers, Layer(Ŵ, b̂, σ))
+    end
+
+    l = network.layers[end]
+    if typeof(l.activation) == NV.Id
+        Ŵ = A * l.weights
+        # use -b, as we want everything to be satisfied, if output ≤ 0 (Ax ≤ b <-> Ax - b ≤ 0)
+        b̂ = A * l.bias - b
+        push!(layers, Layer(Ŵ, b̂, NV.Id()))
+    elseif typeof(l.activation) == NV.ReLU
+        Ŵ = copy(l.weights)
+        b̂ = copy(l.bias)
+        push!(layers, Layer(Ŵ, b̂, NV.ReLU()))
+        # just add an additional layer with the linear objective
+        push!(layers, Layer(A, -b, NV.Id()))
     else
         throw(DomainError(l.activation, ": the activation function is not supported!"))
     end
